b244b2774b864449e5e21e526704edae164d850c
t0b1 <39978666+Kuber12@users.noreply.github.com>
2022-09-06 20:23:58 +0545

    0.2

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var section1 = document.querySelector("#section1");
+var explore = document.querySelector(".explore");
+
+explore.addEventListener("click",function(){
+    section1.style.transform = "translateY(-100%)"
+});
