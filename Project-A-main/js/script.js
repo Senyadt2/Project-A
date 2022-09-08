b1c6314a126df09c2ac507cc260c087cd37bf9e4
t0b1 <39978666+Kuber12@users.noreply.github.com>
2022-09-08 21:12:31 +0545

    Delete Project-A-main directory

--- Project-A-main/js/script.js
+++
@@ -1,6 +0,0 @@
-var section1 = document.querySelector("#section1");
-var explore = document.querySelector(".explore");
-
-explore.addEventListener("click",function(){
-    section1.style.transform = "translateY(-100%)"
-});
